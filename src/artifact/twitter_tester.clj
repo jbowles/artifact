d3f96417622dbbb0f5bb9155e93750cae389329e
joshua bowles <bowlesling@gmail.com>
2012-07-23 09:23:12 -0600

    started twitter-api examples... only works with clojure 1.2.0

--- src/artifact/twitter_tester.clj
+++ src/artifact/twitter_tester.clj
@@ -2,13 +2,11 @@
   [twitter.oauth]
   [twitter.callbacks]
   [twitter.callbacks.handlers]
-  [twitter.api.restful]
+  [twitter.api.restful])
 
-;Access level	 Read-only 
-;About the application permission model
-;Consumer key	E4XIMeoxvEXlmqlOnLt5QQ
-;Consumer secret	UOdbYIlt2nyyIEz2D0bytouaGYk7nIcATrcHvfR8W4o
-;Request token URL	https://api.twitter.com/oauth/request_token
-;Authorize URL	https://api.twitter.com/oauth/authorize
-;Access token URL	https://api.twitter.com/oauth/access_token
-;Callback URL	http://tamlhub.herokuapp.com/streams
+
+
+(def *creds* (make-oauth-creds "" "" "" ""))
+
+(show-user :oauth-creds *creds* :params {:screen-name})
+
